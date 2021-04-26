16c704337d9f292673918bd4a37bc20888b617e9
Iruiasaro <brunettirosario@gmail.com>
2021-04-26 18:12:48 +0200

    completa secondo snack

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//js-snack-1
 /*
 Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
@@ -33,5 +34,43 @@ const bikeChoice = bikes[lightBike];
 const { name, weight } = bikeChoice;
 
 
-console.log(`La Bike più leggera tra quelle contenute all'interno dell'Array è la ${name} con un peso di ${weight} Kg!`)
+console.log(`La Bike più leggera tra quelle contenute all'St.Francisno dell'Array è la ${name} con un peso di ${weight} Kg!`);
 
+
+//js-snack-2
+/*Creare un array di oggetti di squadre di calcio. Ogni newTeam avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0. Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
+Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.*/
+
+const newTeam = [
+    { nome: "Newppy", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Muppet", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Mambo", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "St.Francis", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Flynet", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Hot-Dog", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Artic", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Norfolk", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Shimada", puntiFatti: 0, falliSubiti: 0 },
+];
+
+function randomNumbers(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+
+}
+
+for (var i = 0; i < newTeam.length; i++) {
+    newTeam[i].puntiFatti = randomNumbers(1, 50);
+    newTeam[i].falliSubiti = randomNumbers(1, 30);
+
+}
+
+var nuovoArraySquadre = [];
+
+
+for (var i = 0; i < newTeam.length; i++) {
+    const { nome, falliSubiti } = newTeam[i]
+
+    nuovoArraySquadre.push({nome, falliSubiti });
+}
+
+console.log(nuovoArraySquadre);
